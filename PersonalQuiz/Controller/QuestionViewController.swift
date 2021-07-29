d929640ab6bba56feda90838312ed0a3c770a6be
Kirill Ki <kirillkalapov@gmail.com>
2021-07-29 14:32:33 +0200

    push

--- PersonalQuiz/Controller/QuestionViewController.swift
+++ PersonalQuiz/Controller/QuestionViewController.swift
@@ -32,7 +32,10 @@ class QuestionViewController: UIViewController {
     // MARK: - Private properties
     
     private let questions = Question.getQuestions()
-
+    private var questionIndex = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
@@ -52,11 +55,75 @@ class QuestionViewController: UIViewController {
     */
     
     // MARK: - Private Methods
+    
+    
+    // MARK: - Update UI
     private func updateUI() {
+        // Hide everything
         for stacView in [singleStackView, multiplyStackView, rangedStackView] {
             stacView?.isHidden = true
-            
         }
+        // Get current question
+        let currentQuestion = questions[questionIndex]
+        // Set qurrent question for gurrent label
+        questionLabel.text = currentQuestion.text
+        
+        // Set progress count
+        let totalProgress = Float(questionIndex / questions.count)
+        
+        // Set progress fo questionProgressView
+        questionProgressView.setProgress(totalProgress, animated: true)
+        
+        //Set navigation title
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        let currentAnswer = currentQuestion.answers
+        
+        
+        //Show stack view
+        switch currentQuestion.type {
+        case .single:
+            updataSingleStackView(using: currentAnswer)
+        case .multiply:
+            updateMultiplyStackView(using: currentAnswer)
+        case .ranged:
+            updateRangedStackView(using: currentAnswer)
+        }
+        
+    }
+    
+    
+    
+    /// Setup singel stackciew
+    /// - Parameter answer: array with answers
+    ///
+    /// Description of method
+    private func updataSingleStackView(using answer: [Answer]) {
+        //Show singlestackview
+        singleStackView.isHidden = false
+        
+        for (button, answer) in (singleButtons, answer) {
+            button.setTitle(answer.text, for: .normal)
+        }
+        
+        
+//        for (button, answer) in zip(singleButtons, answers) {
+//            button.setTitle(answer.text, for: .normal)
+//        }
+    }
+    
+    /// Setup multiply stackview
+    /// - Parameter answer: array with answrs
+    private func updateMultiplyStackView(using answer: [Answer]) {
+        
+        multiplyStackView.isHidden = false
+    }
+    
+    /// Setup ranged stackview
+    /// - Parameter answer: array with answrs
+    private func updateRangedStackView(using answer: [Answer]) {
+        
+        rangedStackView.isHidden = false
     }
 
 }
